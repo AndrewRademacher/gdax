7c9e734a3b13adb42cc0e4b49d5dca71e5f8af94
AndrewRademacher <andrewrademacher@gmail.com>
2017-09-25 09:40:50 -0500

    Closes gh-2.

--- src/Main.hs
+++ src/Main.hs
@@ -5,16 +5,54 @@ module Main where
 import           Control.Lens
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
+import           Crypto.Hash
+import           Data.Aeson                 (Value)
+import           Data.Aeson.Encode.Pretty
 import           Data.Aeson.Lens
-import           Data.Text
+import           Data.Byteable
+import           Data.ByteString.Base64
+import qualified Data.ByteString.Char8      as CBS
+import qualified Data.ByteString.Lazy.Char8 as CLBS
+import           Data.Monoid
+import           Data.Text                  (Text)
 import           Data.Time
 import           Data.Time.Clock.POSIX
 import           Network.Wreq
+import           System.Environment
+import           Text.Printf
 
 main :: IO ()
 main = do
     time <- getTime
     print time
+
+    accounts <- listAccounts
+    CLBS.putStrLn $ encodePretty accounts
+
+-- listAccounts - Example of making an authenticated request against GDAX.
+
+listAccounts :: (MonadIO m, MonadThrow m) => m Value
+listAccounts = do
+    accessKey <- liftIO $ CBS.pack <$> getEnv "GDAX_KEY"
+    secretKey <- liftIO $ decodeLenient . CBS.pack <$> getEnv "GDAX_SECRET"
+    passphrase <- liftIO $ CBS.pack <$> getEnv "GDAX_PASSPHRASE"
+    time <- liftIO getCurrentTime
+
+    let timestamp = CBS.pack $ printf "%.0f" (realToFrac (utcTimeToPOSIXSeconds time) :: Double)
+        sigString = timestamp <> "GET" <> "/accounts"
+        sig = encode $ toBytes (hmac secretKey sigString :: HMAC SHA256)
+        opts = defaults
+                & header "CB-ACCESS-KEY" .~ [ accessKey ]
+                & header "CB-ACCESS-SIGN" .~ [ sig ]
+                & header "CB-ACCESS-TIMESTAMP" .~ [ timestamp ]
+                & header "CB-ACCESS-PASSPHRASE" .~ [ passphrase ]
+
+    res <- liftIO $ getWith opts "https://api.gdax.com/accounts"
+    case res ^? responseBody . _Value of
+        Nothing -> throwM $ MalformedGDAXResponse "Account response body was invalid."
+        Just val -> return val
+
+-- getTime - Example of making an unauthenticated request against GDAX.
 
 getTime :: (MonadIO m, MonadThrow m) => m UTCTime
 getTime = do
