9a2777167d1c7fb661b7d77bd9975506c7d8d65b
AndrewRademacher <andrewrademacher@gmail.com>
2017-09-26 10:14:22 -0500

    Successfully connected to a websocket server.

--- app/Sandbox.hs
+++ app/Sandbox.hs
@@ -1,33 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Control.Concurrent
+import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.Aeson                 (Value)
 import           Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Base64     as Base64
 import qualified Data.ByteString.Char8      as CBS
 import qualified Data.ByteString.Lazy.Char8 as CLBS
+import           Data.Text                  (Text)
+import qualified Data.Text                  as T
 import           Network.GDAX.Explicit
+import           Network.WebSockets
 import           System.Environment
+import           Wuss
 
 main :: IO ()
-main = do
-    gAccessKey <- CBS.pack <$> getEnv "GDAX_KEY"
-    gSecretKey <- Base64.decodeLenient . CBS.pack <$> getEnv "GDAX_SECRET"
-    gPassphrase <- CBS.pack <$> getEnv "GDAX_PASSPHRASE"
-
-    g <- mkSandboxGdax gAccessKey gSecretKey gPassphrase
-
-    putStrLn "getTime:"
-    time <- getTime g
-    print time
-
-    putStrLn "listAccounts:"
-    accounts <- listAccounts g
-    CLBS.putStrLn $ encodePretty accounts
-
-    putStrLn "placeOrder:"
-    ores <- placeOrder g
-    CLBS.putStrLn $ encodePretty ores
+main = putStrLn "For use with GHCi."
 
 withGdax :: (MonadIO m) => (Gdax -> m a) -> m a
 withGdax f = do
@@ -41,3 +32,23 @@ withGdax f = do
 
 printPrettyLn :: (MonadIO m) => Value -> m ()
 printPrettyLn = liftIO . CLBS.putStrLn . encodePretty
+
+subscribeSocket :: IO ()
+subscribeSocket = runSecureClient "echo.websocket.org" 443 "/" ws
+
+ws :: ClientApp ()
+ws connection = do
+    putStrLn "Connected."
+
+    void . forkIO . forever $ do
+        msg <- receiveData connection
+        print (msg :: Text)
+
+    let loop = do
+            line <- getLine
+            unless (null line) $ do
+                sendTextData connection (T.pack line)
+                loop
+    loop
+
+    sendClose connection (T.pack "Bye!")
