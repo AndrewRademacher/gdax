690f1716771854e00b71bdd346430411ab204937
AndrewRademacher <andrewrademacher@gmail.com>
2017-09-27 09:33:28 -0500

    Renamed sandbox Gdax config creation components.

--- app/Sandbox.hs
+++ app/Sandbox.hs
@@ -21,11 +21,11 @@ import           Wuss
 main :: IO ()
 main = putStrLn "For use with GHCi."
 
-withGdax :: (MonadIO m) => (Gdax -> m a) -> m a
-withGdax f = do
-    gAccessKey <- liftIO $ CBS.pack <$> getEnv "GDAX_KEY"
-    gSecretKey <- liftIO $ Base64.decodeLenient . CBS.pack <$> getEnv "GDAX_SECRET"
-    gPassphrase <- liftIO $ CBS.pack <$> getEnv "GDAX_PASSPHRASE"
+withSandboxGdax :: (MonadIO m) => (Gdax -> m a) -> m a
+withSandboxGdax f = do
+    gAccessKey <- liftIO $ CBS.pack <$> getEnv "GDAX_SANDBOX_KEY"
+    gSecretKey <- liftIO $ Base64.decodeLenient . CBS.pack <$> getEnv "GDAX_SANDBOX_SECRET"
+    gPassphrase <- liftIO $ CBS.pack <$> getEnv "GDAX_SANDBOX_PASSPHRASE"
 
     g <- mkSandboxGdax gAccessKey gSecretKey gPassphrase
 
@@ -33,36 +33,3 @@ withGdax f = do
 
 printPrettyLn :: (MonadIO m) => Value -> m ()
 printPrettyLn = liftIO . CLBS.putStrLn . encodePretty
-
-subscribeSocket :: IO ()
-subscribeSocket = runSecureClient "ws-feed.gdax.com" 443 "/" client
-
-testSub :: Subscribe
-testSub = Subscribe $ Subscriptions [] [ChannelSubscription ChannelHeartbeat ["BTC-USD"]]
-
-testUnSub :: UnSubscribe
-testUnSub =  UnSubscribe $ Subscriptions [] [ChannelSubscription ChannelHeartbeat ["BTC-USD"]]
-
-client :: ClientApp ()
-client conn = do
-    putStrLn "Connection opened.."
-
-    sendTextData conn (Aeson.encode testSub)
-    void . forkIO . forever $ do
-        msg <- receiveData conn
-        let hbeat = Aeson.decode msg :: Maybe Heartbeat
-        case hbeat of
-            Nothing ->
-                let subs = Aeson.decode msg :: Maybe Subscriptions
-                in case subs of
-                    Nothing -> print ("not sub"::String) >> print msg
-                    Just v  -> print v
-            Just v  -> print v
-
-    threadDelay 1000000
-
-    putStrLn "Unsubscribing..."
-    sendTextData conn (Aeson.encode testUnSub)
-
-    threadDelay 1000000
-    putStrLn "Closing..."
