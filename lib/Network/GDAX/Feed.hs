508765d731d7f49b49425f659065aec10f14d820
AndrewRademacher <andrewrademacher@gmail.com>
2017-10-02 09:50:07 -0500

    Added exports to Network.GDAX.Feed.

--- lib/Network/GDAX/Feed.hs
+++ lib/Network/GDAX/Feed.hs
@@ -1,10 +1,19 @@
-module Network.GDAX.Feed where
+module Network.GDAX.Feed
+    ( module Core
+    , module Exceptions
+    , module Feed
+    , module Shared
+
+    , defaultClient
+    ) where
 
 import           Control.Monad.Catch
-import qualified Data.Aeson              as Aeson
-import qualified Data.Text               as T
-import           Network.GDAX.Exceptions
-import           Network.GDAX.Types.Feed
+import qualified Data.Aeson                as Aeson
+import qualified Data.Text                 as T
+import           Network.GDAX.Core         as Core
+import           Network.GDAX.Exceptions   as Exceptions
+import           Network.GDAX.Types.Feed   as Feed
+import           Network.GDAX.Types.Shared as Shared
 import           Network.WebSockets
 
 defaultClient :: Subscriptions -> (GdaxMessage -> IO b) -> Connection -> IO b
